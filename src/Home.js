864f44172b3102b53e2ce9e42708c5ccb6f0019d
yuqi <yuqi.yang@mail.mcgill.ca>
2021-05-11 14:42:57 -0400

    passing props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -12,14 +13,10 @@ const Home = () => {
     },
   ]);
 
+  //props allow to pass from parents component to child components
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 };
