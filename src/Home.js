42c41a4ae66b48a51fe804b20014b1ce307a0bd4
yuqi <yuqi.yang@mail.mcgill.ca>
2021-05-11 14:27:57 -0400

    lesson 10, hook, useState list

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "my new website", body: "lorem ipsm...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsm...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsm...",
+      author: "yuqi",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Home page</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
