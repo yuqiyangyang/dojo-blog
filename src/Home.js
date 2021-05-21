a5be77c00a4d5c76fc1581be87e6c4e2d36be896
yuqi <yuqi.yang@mail.mcgill.ca>
2021-05-21 17:07:29 -0400

    handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,26 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {
           console.log(data);
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
@@ -23,6 +32,7 @@ const Home = () => {
   //props allow to pass from parents component to child components
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
